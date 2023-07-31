ff40001c708070c017e516f30877057da78e998a
Mher Khachatryan <mher.khachatryan.w@gmail.com>
2023-07-31 14:56:12 +0400

    added token

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 import { Telegraf } from 'telegraf'
-// import { message } from "telegraf/filter"
+import { message } from 'telegraf/filters'
+
+import config from './config/config.js'
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 export default {
-
+  BOT_TOKEN: process.env.BOT_CSS_TOKEN
 }
