bf7e7cee49422596fac40e281859c87489e626ac
Mher Khachatryan <mher.khachatryan.w@gmail.com>
2023-08-01 10:00:51 +0400

    init example

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ import { message } from 'telegraf/filters'
 
 import config from './config/config.js'
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-
+const bot = new Telegraf(config.BOT_TOKEN)
+bot.start(ctx => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
