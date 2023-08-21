d4ee7c5637af3c58cf38d097c61f08088a073fb1
Mher Khachatryan <mher.khachatryan.w@gmail.com>
2023-08-21 10:47:42 +0400

    test

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 import { Telegraf } from 'telegraf'
 // import { message } from "telegraf/filter"
 
+console.log('---------------------------------------------rrrrr---------------------------------------------')
+console.log('valodik')
+console.log(JSON.stringify('just a test', null, 2))
